4f3d1dad2b7abad868b7ce059e0e8f4de25ca65e
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-28 23:48:22 +0200

    added upicke and os for json support

--- src/cli.scala
+++ src/cli.scala
@@ -1,6 +1,8 @@
-
+import upickle.default._
+import os._
 
 @main def cli(inputFilePath: String): Unit = {
   println(inputFilePath)
-
+  val jsonString = os.read(os.pwd / inputFilePath)
+  print(jsonString)
 }
