a8c797f448b4b6924bb3cc12c363221952e0fd5f
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-31 22:00:55 +0200

    added doodle to dependencies

--- src/main/scala/com/dev/main.scala
+++ src/main/scala/com/dev/main.scala
@@ -1,9 +1,37 @@
 package com.dev
 
 import com.dev.cli.InputFile
+import doodle.image.*
+import doodle.core.*
+import doodle.core.format.Png
+
+// Extension methods
+import doodle.image.syntax.all.*
+// Render to a window using Java2D (must be running in the JVM)
+import doodle.java2d.*
+// Need the Cats Effect runtime to run everything
+import cats.effect.unsafe.implicits.global
 
 
 @main def main(inputFilePath: String): Unit = {
+  val img = Image
+    .rectangle(800,100).fillColor(Color.gray)
+    .above(Image.rectangle(800,10).fillColor(Color.lightGray)
+      .above(Image.rectangle(800,10).fillColor(Color.gray)))
+
+  val images = List(
+    Image.rectangle(800,100).fillColor(Color.gray),
+    Image.rectangle(800,10).fillColor(Color.lightGray),
+    Image.rectangle(800,100).fillColor(Color.gray)
+  )
+
+  // Stack images dynamically
+  val stacked = images.reduce(_.above(_))
+
+
+  stacked.draw()
+
+
   println(inputFilePath)
 
   for
