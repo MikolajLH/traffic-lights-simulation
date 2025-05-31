09e4a6df33e7e8718b3240c7756e9bdf0430102f
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-31 23:29:29 +0200

    added example of RoadBuilder usage in main

--- src/main/scala/com/dev/main.scala
+++ src/main/scala/com/dev/main.scala
@@ -1,13 +1,23 @@
 package com.dev
 
 import com.dev.cli.InputFile
+import com.dev.simulation.junction.TrafficDirection.{forward, left}
+import com.dev.simulation.junction.{Crossing, Lane, RoadBuilder}
 
 
 @main def main(inputFilePath: String): Unit = {
   println(inputFilePath)
 
+  val roadResult = RoadBuilder()
+    .push(Crossing())
+    .flatMap(_.push(Lane(Set(forward))))
+    .flatMap(_.push(Lane(Set(left))))
+    .flatMap(_.push(Crossing()))
+    .flatMap(_.build)
+
   for
     commands <- InputFile.parse(inputFilePath)
+    road <- roadResult
   do
     commands.flatten.foreach( cmd => cmd.introduce())
 }
