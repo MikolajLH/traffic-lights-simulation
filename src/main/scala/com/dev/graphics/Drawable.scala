6f94ea609739f16cf68e84a80d5a106b12d0af4b
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-06-03 23:25:08 +0200

    fixed crossing display

--- src/main/scala/com/dev/graphics/Drawable.scala
+++ src/main/scala/com/dev/graphics/Drawable.scala
@@ -29,13 +29,40 @@ given Drawable[Set[TrafficLightsDirection]] with
   }.reduce(_.on(_)).transform(Transform.rotate(270.degrees))
 
 given Drawable[Lane] with
-  extension (a: Lane) def toImage: Image = a.to.toImage.transform(Transform.scale(0.1, 0.1)).on(Image.rectangle(200, 20).fillColor(Color.gray))
+  extension (a: Lane) def toImage: Image = {
+    Image.rectangle(200, 48).fillColor(Color.darkGray).strokeColor(Color.darkGray)
+  }
 
 given Drawable[Crossing] with
-  extension (a: Crossing) def toImage: Image = Image.rectangle(200,20).fillColor(Color.azure)
+  extension (a: Crossing) def toImage: Image = {
+    Image.rectangle(200,20).fillColor(Color.lightGray)
+  }
 
 given Drawable[Road] with
-  extension (r: Road) def toImage: Image = r.elements.map(_.toImage).reduce(_.above(_))
+  extension (r: Road) def toImage: Image = {
+    var laneCounter = 0
+    var isCrossing = false
+    val zebra = (for _ <- 1 to 4 yield List(Image.rectangle(30, 5).fillColor(Color.white).strokeColor(Color.white), Image.rectangle(30, 4).fillColor(Color.darkGray).strokeColor(Color.darkGray))).flatten.reduce(_.below(_)).below(Image.rectangle(30, 4).fillColor(Color.white).strokeColor(Color.white))
+    val parts = for
+      el <- r.elements
+    yield {
+        el match {
+          case l: Lane =>
+            laneCounter = laneCounter + 1
+            val limg = if isCrossing then zebra.at(50,0).on(l.toImage) else l.toImage
+            if laneCounter > 0
+            then List(limg, Image.rectangle(200,2).fillColor(Color.lightGray).strokeColor(Color.lightGray))
+            else List(limg)
+          case _ =>
+            laneCounter = 0
+            isCrossing = true
+            List(Image.rectangle(200,2).fillColor(Color.red).strokeColor(Color.black))
+        }
+      }
+    println(laneCounter)
+    parts.flatten.reduce(_.above(_))
+    //r.elements.map(_.toImage).reduce(_.above(_))
+  }
 
 
 
